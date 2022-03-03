52e6fbdf6a90991e93e8afac58e235afbc32e683
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-03 09:22:10 +0000

    Ahora ya funciona correctamente

--- ejercicio1.py
+++ ejercicio1.py
@@ -14,6 +14,6 @@ def dicotomia(numero_medio):
     dicotomia(numero_medio)
 dicotomia(numero_medio)
 lista1_reversed=(lista1[::-1]) #copiado de stackoverflow, ya que el reverse() no me funcionaba
-listafinal.append(lista1.pop(0))
+listafinal.append(lista2.pop(0))
 listafinal=listafinal+ lista1_reversed + lista2
 print(f"La lista final es {listafinal}")
