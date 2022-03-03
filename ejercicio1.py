c1a10dfd69a3174de484873a946da66ffc8947e2
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-03 09:17:21 +0000

    Funciona correctamente, falta eliminar los paréntesis

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,13 +1,20 @@
-lista_palabra=["Alien","Eduardo","Cabeza","Rubén","Javier","Atlas","Río", "Servir", "Abrir","Camión", "Ordenador", "Mandaloriano", "Conflicto", "Móvil", "Estambul","Wifi", "Nft", "Real Madrid","Dicotomía","Gafas", "Pesado", "Bicicleta","Reloj", "Abrigo", "Criptoestafa", "Emblemático","Inanición","Comunicación","Ifema","Spa","Auditorio","Campo", "Bernabéu", "Iluminati", "Copiar", "Libre", "Como", "El", "Mar"]
-lista_dicotomia=[]
+lista_palabra=["Alien","Eduardo","Cabeza","Rubén","Javier","Atlas","Río", "Servir", "Abrir"]
+lista1=[]
+lista2 = []
+listafinal = []
 print(lista_palabra)
 cota_sup=len(lista_palabra)
 numero_medio=(cota_sup)// 2
 def dicotomia(numero_medio):
-  lista_dicotomia.append(lista_palabra.pop(numero_medio))
   if numero_medio > 0:
+    lista1.append(lista_palabra.pop(numero_medio))
     dicotomia(numero_medio-1)
   if numero_medio == 0 and len(lista_palabra) > 0:
+    lista2.append(lista_palabra.pop(numero_medio))
     dicotomia(numero_medio)
 dicotomia(numero_medio)
-print(f"La lista final es: {lista_dicotomia}")
+lista1_reversed=(lista1[::-1]) #copiado de stackoverflow, ya que el reverse() no me funcionaba
+listafinal.append(lista2.pop(0))
+listafinal.append(lista1_reversed)
+listafinal.append(lista2)
+print(f"La lista final es {listafinal}")
