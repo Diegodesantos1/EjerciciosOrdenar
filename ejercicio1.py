0396004b907731bd3002c2587137c7e33015d14a
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-04 09:36:06 +0000

    todavía no funciona la ordenación por dicotomía

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,11 +1,22 @@
 print("¿Cómo quieres ejecutar el programa? 1 sin tabla, 2 con otra tabla")
 eleccion=int(input())
-lista=[1,2,8,10,2,8,5,0,2,3,7,5,4,7]
-cota_sup=len(lista)
-numero_medio = cota_sup//2
-if eleccion == 1:
-  def ordenar_lista(numero):
-    if len(lista) > 0:
-    
-ordenar_lista(numero)
-  
+
+lista_palabra=[2,45,67,18,13,9,98,78,5,65,77,1]
+lista1=[]
+lista2 = []
+listafinal = []
+print(lista_palabra)
+cota_sup=len(lista_palabra)
+numero_medio=(cota_sup)// 2
+def dicotomia(numero_medio):
+  if numero_medio > 0:
+    lista1.append(lista_palabra.pop(numero_medio))
+    dicotomia(numero_medio-1)
+  if numero_medio == 0 and len(lista_palabra) > 0:
+    lista2.append(lista_palabra.pop(numero_medio))
+    dicotomia(numero_medio)
+dicotomia(numero_medio)
+lista1_reversed=(lista1[::-1]) #copiado de stackoverflow, ya que el reverse() no me funcionaba
+listafinal.append(lista2.pop(0))
+listafinal=listafinal+ lista1_reversed + lista2
+print(f"La lista final es {listafinal}")
