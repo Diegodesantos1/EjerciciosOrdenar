87109fe4b7e8483e977f7866920dd483a78b22c3
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-04 09:26:18 +0000

    planteado el ejercicio

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,11 @@
-lista_numeros=[1,2,8,13,2,8,5,0,2,3,7,5,4,7]
-
+print("¿Cómo quieres ejecutar el programa? 1 sin tabla, 2 con otra tabla")
+eleccion=int(input())
+lista=[1,2,8,10,2,8,5,0,2,3,7,5,4,7]
+cota_sup=len(lista)
+numero_medio = cota_sup//2
+if eleccion == 1:
+  def ordenar_lista(numero):
+    if len(lista) > 0:
+    
+ordenar_lista(numero)
+  
