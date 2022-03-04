ebc98f2f99cde4ebf75b11c4804ca7fedbab2945
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-04 18:56:22 +0000

    Por ahora funciona el algoritmo, falta aplicar la recursividad para llenar el diccionario

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,5 @@
 import random
+
 lista_tareas = []
 def crear_tareas():
   if len(lista_tareas) < 5:
@@ -7,7 +8,7 @@ def crear_tareas():
     if i >= j:
       crear_tareas()
     else:
-      tarea = (i,j)
+      tarea = f"{i,j}"
       lista_tareas.append(tarea)
       crear_tareas()
   else:
@@ -19,3 +20,11 @@ def ordenar_tareas():
   lista_tareas.sort()
   print(lista_tareas)
 ordenar_tareas()
+def nombrar_tareas():
+  tareas =["Fregar", "Aspirar", "Poner la lavadora", "Hacer la comida", "Limpiar los baños", "Tender la ropa", "Limpiar el polvo","Recoger la habitación", "Ordenar las estanterías", "Sacar el lavavajillas"]
+  numero_tareas = random.randint(1,10)
+  tarea_escrita=tareas.pop(numero_tareas)
+  tarea_numero = lista_tareas.pop(0)
+  diccionario={tarea_escrita: tarea_numero}
+  print(diccionario)
+nombrar_tareas()
