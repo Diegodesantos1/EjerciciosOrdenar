85bcccaf2a80081218df43b659ec8b7922fb8da9
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-04 19:14:52 +0000

    CReado el diccionario de las tareas con su orden de prioridad

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,7 +2,7 @@ import random
 
 lista_tareas = []
 def crear_tareas():
-  if len(lista_tareas) < 5:
+  if len(lista_tareas) < 10:
     i = random.randint(1,10)
     j = random.randint(1,10)
     if i >= j:
@@ -20,11 +20,14 @@ def ordenar_tareas():
   lista_tareas.sort()
   print(lista_tareas)
 ordenar_tareas()
+
+tareas =["Fregar", "Aspirar", "Poner la lavadora", "Hacer la comida", "Limpiar los baños", "Tender la ropa", "Limpiar el polvo","Recoger la habitación", "Ordenar las estanterías", "Sacar el lavavajillas"]
+diccionario = {}
 def nombrar_tareas():
-  tareas =["Fregar", "Aspirar", "Poner la lavadora", "Hacer la comida", "Limpiar los baños", "Tender la ropa", "Limpiar el polvo","Recoger la habitación", "Ordenar las estanterías", "Sacar el lavavajillas"]
-  numero_tareas = random.randint(1,10)
-  tarea_escrita=tareas.pop(numero_tareas)
-  tarea_numero = lista_tareas.pop(0)
-  diccionario={tarea_escrita: tarea_numero}
-  print(diccionario)
+  for n in range (10):
+    tarea_escrita=tareas.pop(0)
+    tarea_numero = lista_tareas.pop(0)
+    diccionario[tarea_escrita] = tarea_numero
 nombrar_tareas()
+lista_tareas_ordenada=diccionario.keys()
+print(f"Las tareas son en orden de prioridad: {lista_tareas_ordenada}")
