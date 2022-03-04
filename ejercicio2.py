b391456f360fa6bd72e843e4bff90b5eee4bca8c
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-04 14:31:36 +0000

    creada la función de crear tareas aplicando recursividad

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,14 @@
-
-  
+import random
+lista_tareas= []
+def crear_tareas():
+  if len(lista_tareas) < 10:
+    i = random.randint(1,100)
+    j = random.randint(1,100)
+    tarea =  (i,j)
+    lista_tareas.append(tarea)
+    crear_tareas()
+  else:
+    return lista_tareas
+crear_tareas()
+print(lista_tareas)
   
