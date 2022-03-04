99c1951c437b167406472621736cd0a18811691b
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-04 12:20:27 +0000

    Cambios menores y resolución del ejercicio

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,31 +1,28 @@
 import random
-print("¿Cómo quieres ejecutar el programa? 1 sin tabla, 2 con otra tabla")
-eleccion=int(input())
 lista1=[]
 lista2 = []
 listafinal = []
-if eleccion == 2:
-  lista_numero=[]
-  def crear_lista():
-    lista_numero.sort()
-    if len(lista_numero) < 20:
-      numero=random.randint(0,100)
-      lista_numero.append(numero)
-      crear_lista()
-    else:
-      return lista_numero
-  crear_lista()
-  print(f"La lista inicial es: {lista_numero}")
-  cota_sup=len(lista_numero)
-  numero_medio=(cota_sup)// 2
-  def dicotomia(numero_medio):
-    if numero_medio > 0:
-      lista1.append(lista_numero.pop(numero_medio))
-      dicotomia(numero_medio-1)
-    if numero_medio == 0 and len(lista_numero) > 0:
-      lista2.append(lista_numero.pop(numero_medio))
-      dicotomia(numero_medio)
-  dicotomia(numero_medio)
-  listafinal=listafinal+ lista1 + lista2
-  print(f"La lista final con la dicotomía es {listafinal}")
+lista_numero=[]
+def crear_lista():
+  lista_numero.sort()
+  if len(lista_numero) < 20:
+    numero=random.randint(0,100)
+    lista_numero.append(numero)
+    crear_lista()
+  else:
+    return lista_numero
+crear_lista()
+print(f"La lista inicial es: {lista_numero}")
+cota_sup=len(lista_numero)
+numero_medio=(cota_sup)// 2
+def dicotomia(numero_medio):
+  if numero_medio > 0:
+    lista1.append(lista_numero.pop(numero_medio))
+    dicotomia(numero_medio-1)
+  if numero_medio == 0 and len(lista_numero) > 0:
+    lista2.append(lista_numero.pop(numero_medio))
+    dicotomia(numero_medio)
+dicotomia(numero_medio)
+listafinal=listafinal+ lista1 + lista2
+print(f"La lista final con la dicotomía es {listafinal}")
   
