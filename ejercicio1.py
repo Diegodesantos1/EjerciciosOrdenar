c89374ec6d8fd79181a69be37e923d8b7672a865
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-04 11:01:35 +0000

    aplicada la dicotomía

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,7 @@
 print("¿Cómo quieres ejecutar el programa? 1 sin tabla, 2 con otra tabla")
 eleccion=int(input())
 if eleccion == 2:
-  lista_numero=[2,5,10,7,3,5,0,1,3]
+  lista_numero=[1,2,3,4,5,6,7,8,9,10]
   lista1=[]
   lista2 = []
   listafinal = []
@@ -16,6 +16,5 @@ if eleccion == 2:
       lista2.append(lista_numero.pop(numero_medio))
       dicotomia(numero_medio)
   dicotomia(numero_medio)
-  listafinal.append(lista2.pop(0))
   listafinal=listafinal+ lista1 + lista2
   print(f"La lista final es {listafinal}")
