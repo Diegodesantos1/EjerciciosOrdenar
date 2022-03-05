2fed39fc35bebf9ea67ea745b222fa51fcc7b3c5
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-05 18:25:16 +0000

    Creada la función de crear la lista con números aleatorios

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,3 +1,14 @@
-
+import random
+lista_numero=[]
+def crear_lista():
+  lista_numero.sort()
+  if len(lista_numero) < 20:
+    numero=random.randint(0,100)
+    lista_numero.append(numero)
+    crear_lista()
+  else:
+    return lista_numero
+crear_lista()
+print(f"La lista inicial es: {lista_numero}")
     
     
