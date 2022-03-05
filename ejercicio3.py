19674c59d4b7ddfef964fa468e816c8cff05ee17
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-05 23:21:13 +0000

    creadas varias listas de segmentos y probado el funcionamiento básico

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,8 +1,12 @@
 import random
 lista_numero=[]
-lista_segmento=[]
+lista_segmento = []
+lista_segmento2 = []
+lista_segmento3 = []
+lista_segmento4 = []
+lista_segmento5 = []
 def crear_lista():
-  if len(lista_numero) < 20:
+  if len(lista_numero) < 10:
     numero=random.randint(0,100)
     lista_numero.append(numero)
     crear_lista()
@@ -13,11 +17,25 @@ print(f"La lista inicial es: {lista_numero}")
 posicion_numero_max = lista_numero.index(max(lista_numero))
 def crear_segmentos(posicion_numero_max):
   if lista_numero[posicion_numero_max] > lista_numero[posicion_numero_max + 1]:
-    lista_segmento.append(lista_numero.pop(posicion_numero_max))
-    lista_segmento.append(lista_numero.pop(posicion_numero_max + 1))
+    for i in range (2): 
+      lista_segmento.append(lista_numero.pop(posicion_numero_max))
     print(lista_segmento)
-    crear_segmentos(posicion_numero_max + 1)
-  else:
+    if posicion_numero_max < 1 + len(lista_numero):
+      crear_segmentos(posicion_numero_max)
+    else:
+      crear_segmentos(posicion_numero_max = 0)
       return (lista_segmento)
+  elif lista_numero[posicion_numero_max] < lista_numero[posicion_numero_max + 1]:
+    lista_segmento2.append(lista_numero.pop(posicion_numero_max))
+    print(lista_segmento2)
+  elif lista_numero[posicion_numero_max] < lista_numero[posicion_numero_max + 1]:
+    lista_segmento3.append(lista_numero.pop(posicion_numero_max))
+    print(lista_segmento3)
+  elif lista_numero[posicion_numero_max] < lista_numero[posicion_numero_max + 1]:
+    lista_segmento4.append(lista_numero.pop(posicion_numero_max))
+    print(lista_segmento4)
+  elif lista_numero[posicion_numero_max] < lista_numero[posicion_numero_max + 1]:
+    lista_segmento5.append(lista_numero.pop(posicion_numero_max))
+    print(lista_segmento5)
 crear_segmentos(posicion_numero_max)
   
