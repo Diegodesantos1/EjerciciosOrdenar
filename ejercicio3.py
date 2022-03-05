f9699bd878a5347dbe1e0eb157e4f352365f6b94
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-03-05 18:51:44 +0000

    Planetada la función de crear segmentos

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,7 +1,7 @@
 import random
 lista_numero=[]
+lista_segmento=[]
 def crear_lista():
-  lista_numero.sort()
   if len(lista_numero) < 20:
     numero=random.randint(0,100)
     lista_numero.append(numero)
@@ -10,5 +10,15 @@ def crear_lista():
     return lista_numero
 crear_lista()
 print(f"La lista inicial es: {lista_numero}")
-    
-    
+
+def crear_segmentos():
+  numero_max = max(lista_numero)
+  posicion_numero_max =(lista_numero.index(numero_max))
+  while len(lista_numero):
+    if lista_numero[posicion_numero_max] < lista_numero[posicion_numero_max + 1]:
+      lista_segmento.append(lista_numero[posicion_numero_max + 1])
+    else:
+      return (lista_segmento)
+crear_segmentos()
+  
+print(lista_segmento)
